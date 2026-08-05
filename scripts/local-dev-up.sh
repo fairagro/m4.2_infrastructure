77797d71b5b445f5ce53e263e46349615359d159
Carsten Scharfenberg <carsten.scharfenberg@zalf.de>
2026-08-05 09:49:06 +0000

    Update local-dev-up.sh to force-update Helm repo for postgres-operator installation
    This commit modifies the Helm command in local-dev-up.sh to include the --force-update flag when adding the postgres-operator-charts repository. This change ensures that the repository is updated even if it already exists, improving the reliability of the installation process.

--- scripts/local-dev-up.sh
+++ scripts/local-dev-up.sh
@@ -55,7 +55,7 @@ if kubectl get crd postgresqls.acid.zalan.do >/dev/null 2>&1 \
   echo "postgres-operator already present."
 else
   echo "Installing Zalando postgres-operator (Helm) ..."
-  helm repo add postgres-operator-charts https://opensource.zalando.com/postgres-operator/charts/postgres-operator >/dev/null
+  helm repo add --force-update postgres-operator-charts https://opensource.zalando.com/postgres-operator/charts/postgres-operator >/dev/null
   helm repo update postgres-operator-charts >/dev/null
   # Defaults are fine for kind: Teams API off, watch all namespaces (teamId fairagro needs no team API).
   helm upgrade --install postgres-operator postgres-operator-charts/postgres-operator \
